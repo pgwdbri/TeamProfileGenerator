27e7c84f676f9daefd2023cf79f7d91b3d125855
Bri <bri.krystalle@gmail.com>
2021-03-27 22:40:13 -0400

    notes

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 const Manager = require("./class/Manager");
-
 const Engineer = require("./class/Engineer");
 const Intern = require("./class/Intern");
 const Employee = require("./class/Employee");
+//creating the variables of the team members, refering to a class file
 
 const employees = [];
 
@@ -18,15 +18,17 @@ function initApp() {
     startHtml();
     addMember();
 }
+//start and add member
 
 function addMember() {
     inquirer.prompt([{
-        message: "Enter team member's name",
+      //target inquirer
+        message: "Enter the name of a team member",
         name: "name"
     },
     {
         type: "list",
-        message: "Select team member's role",
+        message: "Enter the role of the team member",
         choices: [
             "Engineer",
             "Intern",
@@ -35,32 +37,39 @@ function addMember() {
         name: "role"
     },
     {
-        message: "Enter team member's id",
+        message: "Enter team member id",
         name: "id"
     },
     {
-        message: "Enter team member's email address",
+        message: "Enter the email address of the team member",
         name: "email"
     }])
+    //questions to ask for info
+
+    //function to ask based on role
     .then(function({name, role, id, email}) {
         let roleInfo = "";
+
         if (role === "Engineer") {
             roleInfo = "GitHub username";
-        } else if (role === "Intern") {
+        } 
+        else if (role === "Intern") {
             roleInfo = "school name";
-        } else {
-            roleInfo = "office phone number";
+        } 
+        else {
+            roleInfo = "phone number";
         }
+
         inquirer.prompt([{
-            message: `Enter team member's ${roleInfo}`,
+            message: `Enter team member role ${roleInfo}`,
             name: "roleInfo"
         },
         {
             type: "list",
-            message: "Would you like to add more team members?",
+            message: "Are there more team members?",
             choices: [
-                "yes",
-                "no"
+                "Yes",
+                "No"
             ],
             name: "moreMembers"
         }])
@@ -68,11 +77,14 @@ function addMember() {
             let newMember;
             if (role === "Engineer") {
                 newMember = new Engineer(name, id, email, roleInfo);
-            } else if (role === "Intern") {
+            } 
+            else if (role === "Intern") {
                 newMember = new Intern(name, id, email, roleInfo);
-            } else {
+            } 
+            else {
                 newMember = new Manager(name, id, email, roleInfo);
             }
+
             employees.push(newMember);
             addHtml(newMember)
             .then(function() {
