9c66da5615f66f63de0963fa51de473dd10df28f
Bri <bri.krystalle@gmail.com>
2021-03-27 22:44:54 -0400

    html if statements

--- script.js
+++ script.js
@@ -99,3 +99,116 @@ function addMember() {
     });
 }
 
+
+//html portion
+
+//generate html document
+function startHtml() {
+  const html = `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <title>Team Profile</title>
+  </head>
+
+  <body>
+      <div class="container">
+          <div class="row">`;
+  fs.writeFile('index.html', html, function(err) {
+      if (err) {
+          console.log(err);
+      }
+  });
+
+  console.log("start");
+}
+
+//adding a member to the html document
+function addHtml(member) {
+
+  return new Promise(function(resolve, reject) {
+
+      const name = member.getName();
+      const role = member.getRole();
+      const id = member.getId();
+      const email = member.getEmail();
+
+      let data = "";
+
+      //eningeer needs a specific question, need to make an if statement for it 
+      if (role === "Engineer") {
+          const gitHub = member.getGithub();
+          data = 
+          `<div class="col-6">
+          <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Engineer</h5>
+              <ul class="list-group list-group-flush">
+                  <li class="list-group-item">ID: ${id}</li>
+                  <li class="list-group-item">Email Address: ${email}</li>
+                  <li class="list-group-item">GitHub: ${gitHub}</li>
+              </ul>
+            </div>
+          </div>`;
+
+      } 
+      //if statement for intern
+      else if (role === "Intern") {
+          const school = member.getSchool();
+          data = 
+          `<div class="col-6">
+          <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Intern</h5>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: ${email}</li>
+                <li class="list-group-item">School: ${school}</li>
+              </ul>
+            </div>
+          </div>`;
+      } 
+      
+      else {
+          const officePhone = member.getOfficeNumber();
+          data = `<div class="col-6">
+          <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Manager</h5>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: ${email}</li>
+                <li class="list-group-item">Office Phone: ${officePhone}</li>
+              </ul>
+            </div>
+          </div>`
+      }
+
+      
+      fs.appendFile('index.html', data, function (err) {
+          if (err) {
+              return reject(err);
+          };
+          return resolve();
+      });
+  });
+}
+
+function finishHtml() {
+  const html = ` 
+  </div>
+  </div>
+  </body>
+  </html>`;
+
+  fs.appendFile('index.html', html, function (err) {
+      if (err) {
+          console.log(err);
+      };
+  });
+}
+
+initApp();
+
+
+
